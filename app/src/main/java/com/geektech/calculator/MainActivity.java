c3f539e2258b5c0171a4170a27e532c592592931
SanzharAizhigitov <ajzigitovsanzar@gmail.com>
2022-11-25 17:52:23 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/calculator/MainActivity.java
@@ -0,0 +1,151 @@
+package com.geektech.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+    }
+
+    Integer first, second, result;
+    TextView textView;
+    boolean isOperationClick;
+    Operetion operetion;
+
+
+    public void onNumberClick(View view) {
+        switch (view.getId()) {
+            case R.id.one:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("1");
+                } else {
+                    textView.append("1");
+                }
+                break;
+            case R.id.two:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("2");
+                } else {
+                    textView.append("2");
+                }
+                break;
+            case R.id.three:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("3");
+                } else {
+                    textView.append("3");
+                }
+                break;
+            case R.id.four:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("4");
+                } else {
+                    textView.append("4");
+                }
+                break;
+            case R.id.five:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("5");
+                } else {
+                    textView.append("5");
+                }
+                break;
+            case R.id.six:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("6");
+                } else {
+                    textView.append("6");
+                }
+                break;
+            case R.id.seven:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("7");
+                } else {
+                    textView.append("7");
+                }
+                break;
+            case R.id.eight:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("8");
+                } else {
+                    textView.append("8");
+                }
+                break;
+            case R.id.nine:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("9");
+                } else {
+                    textView.append("9");
+                }
+                break;
+            case R.id.zero:
+                if (textView.getText().toString().equals("0") || isOperationClick) {
+                    textView.setText("0");
+                } else {
+                    textView.append("0");
+                }
+                break;
+            case R.id.AC:
+                textView.setText("0");
+                first = 0;
+                second = 0;
+        }
+        isOperationClick = false;
+    }
+
+    public void onOperationClick(View view) {
+        switch (view.getId()) {
+            case R.id.plus:
+                first = Integer.valueOf(textView.getText().toString());
+                operetion=Operetion.PLUS;
+                break;
+            case R.id.minus:
+                first = Integer.valueOf(textView.getText().toString());
+                operetion=Operetion.MINIS;
+
+                break;
+            case R.id.delit:
+                operetion=Operetion.DELIT;
+
+                first = Integer.valueOf(textView.getText().toString());
+                break;
+            case R.id.multipli:
+                operetion=Operetion.MULTIPLY;
+
+                first = Integer.valueOf(textView.getText().toString());
+                break;
+            case R.id.ravna:
+                if (operetion == Operetion.PLUS) {
+                    second = Integer.valueOf(textView.getText().toString());
+                    result = first + second;
+                    textView.setText(result.toString());
+                }
+               else if (operetion == Operetion.MINIS) {
+                    second = Integer.valueOf(textView.getText().toString());
+                    result = first - second;
+                    textView.setText(result.toString());
+                }
+                else if (operetion == Operetion.DELIT) {
+                    second = Integer.valueOf(textView.getText().toString());
+                    result = first/ second;
+                    textView.setText(result.toString());
+                }
+                if (operetion == Operetion.MULTIPLY) {
+                    second = Integer.valueOf(textView.getText().toString());
+                    result = first * second;
+                    textView.setText(result.toString());
+                }
+
+
+                break;
+        }
+        isOperationClick = true;
+    }
+}
---
+++ app/src/main/java/com/geektech/calculator/Operetion.java
@@ -0,0 +1,5 @@
+package com.geektech.calculator;
+
+public enum Operetion {
+    MINIS,PLUS,DELIT,MULTIPLY
+}
